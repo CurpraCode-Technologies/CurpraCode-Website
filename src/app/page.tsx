2ed822460dbbbe6069b4a7efc4ab24f9f7160b4f
CurpraCode <curtsoyaks@gmail.com>
2024-09-29 21:58:52 +0100

    edit page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus error dicta explicabo perspiciatis mollitia, unde repellat! Aspernatur ea earum quos eligendi id, tempore magni. Officia quis ratione rerum quidem in?</p>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
