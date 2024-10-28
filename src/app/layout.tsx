7cd798029965bd6fb8918c13ac5bec0dbd6fe45e
curtizdev <curtsoyaks@gmail.com>
2024-10-28 21:44:41 +0100

    fixinx google indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MC6TC8NV');`}
+        </script>
+
         {/* Google Tag Manager */}
         <script
           async
@@ -123,6 +131,15 @@ export default function RootLayout({
       <body
         className={`${stacionRegular.variable} ${productSans.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MC6TC8NV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
       </body>
       <GoogleAnalytics gaId="G-SJYW7J6Z3W" />
